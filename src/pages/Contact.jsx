be396ec159c27d4e1cff44962a8f27fe7ea108df
rinoykj007 <rinoykj007@gmail.com>
2025-11-22 21:08:51 +0300

    updat location

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -455,7 +455,7 @@ export default function Contact() {
             className="rounded-2xl overflow-hidden shadow-xl"
           >
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.5!2d74.0!3d16.7!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTbCsDQyJzAwLjAiTiA3NMKwMDAnMDAuMCJF!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+              src="https://maps.app.goo.gl/PwwND2crM1i6tBeH6"
               width="100%"
               height="450"
               style={{ border: 0 }}
